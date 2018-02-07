a30fd97cbef647afbf3e249c05d8951340a8e56b
meteorchen <meteor614@gmail.com>
2018-02-07 15:10:33 +0800

    init version

---
+++ links_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SCRIPTPATH=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+cd ~
+files=($(ls -Fa ${SCRIPTPATH}| grep '^\..*[^/]$'))
+for i in ${files[@]};do
+    ln -s ${SCRIPTPATH}/${i}
+done
+ln -s ~/.vimrc .vim/init.vim
