491267ba74bcec4d526494cbf06990cea20f5a6c
meteorchen <meteor614@gmail.com>
2018-02-08 21:05:55 +0800

    link dirs

--- links_all.sh
+++ links_all.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 SCRIPTPATH=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 cd ~
-files=($(ls -Fa ${SCRIPTPATH}| grep '^\..*[^/]$'))
+files=($(ls -FA ${SCRIPTPATH}|grep '^\..*$'|grep -v '^\.git/$'))
 for i in ${files[@]}; do
     ln -s ${SCRIPTPATH}/${i}
 done
+# for neovim
 ln -s ~/.vimrc .vim/init.vim
