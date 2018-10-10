57b362ea3bc49f737a0d7e3dce1a6ce73acf59ed
meteorchen <meteorchen@qq.com>
2018-10-10 15:28:57 +0800

    add if

--- bin/generate_tags.sh
+++ bin/generate_tags.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #ctags -R --language-force=c++ --c++-kinds=+plx --fields=+iaS --extra=+q -f ~/cpp_tags /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1
-ctags -R --language-force=c++ --c++-kinds=+plx --fields=+iaS --extra=+q -f ~/cpp_tags `echo | g++ -v -x c++ -E - 2>&1 |grep '^ .*/include/c++\(/[^/]*\)\?$'`
+if type ctags &>/dev/null; then
+    ctags -R --language-force=c++ --c++-kinds=+plx --fields=+iaS --extra=+q -f ~/cpp_tags `echo | g++ -v -x c++ -E - 2>&1 |grep '^ .*/include/c++\(/[^/]*\)\?$'`
+fi
 #echo | g++ -v -x c++ -E -
