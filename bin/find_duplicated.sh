2f0d38db996e51b7f14a701358551183174e1f9e
meteorchen <meteorchen@qq.com>
2019-01-02 17:29:15 +0800

    add find_duplicated.sh

---
+++ bin/find_duplicated.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+find_opt="! -path '*/@eaDir*' ! -path '*/.git/*'"
+find_size_opt=""
+#find_size_opt="-size +4k"
+dir="."
+if [ -n "$1" ]; then
+    dir="$1"
+fi
+
+type parallel >/dev/null 2>&1 || alias parallel='xargs'
+type gfind >/dev/null 2>&1 && alias find='gfind'
+type guniq >/dev/null 2>&1 && alias uniq='guniq'
+
+if type gmd5sum >/dev/null 2>&1 ; then
+    find "${dir}" ${find_opt} ${find_size_opt} -not -empty -type f -printf "%s\n" | sort -rn |uniq -d | parallel -I{} -n1 find "${dir}" ${find_opt} -type f -size {}c -print0 | parallel -0 gmd5sum | sort | uniq -w32 --all-repeated=separate | cut -b 36-
+else
+    find "${dir}" ${find_opt} ${find_size_opt} -not -empty -type f -printf "%s\n" | sort -rn |uniq -d | parallel -I{} -n1 find "${dir}" ${find_opt} -type f -size {}c -print0 | parallel -0 md5sum | sort | uniq -w32 --all-repeated=separate | cut -b 36-
+fi
+
+
+#find ! -path '*/@eaDir*' -not -empty -type f -printf "%s\n" | sort -rn |uniq -d | parallel -I{} -n1 find ! -path '*/@eaDir*' -type f -size {}c -print0 | parallel -0 md5sum | sort | uniq -w32 --all-repeated=separate | cut -b 36-
