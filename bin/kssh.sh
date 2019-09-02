e87f0e90c7133b338dc78b583e566e9599718245
meteorchen <meteorchen@qq.com>
2019-09-02 21:27:06 +0800

    update

--- bin/kssh.sh
+++ bin/kssh.sh
@@ -8,5 +8,11 @@ fi
 pods_patten="$1"
 node_patten="$2"
 nodes=`kubectl get pods -n "${pods_patten}"|grep "${node_patten}"|grep 'Running'|awk '{printf " "$1}'`
-#echo "connecting $nodes"
+arr=($nodes)
+if [ l${#arr[@]} == l0 ];then
+    kubectl config get-contexts
+    echo "node ${node_patten} not found, try to contexts use command: kubectl config use-context NAME"
+    exit
+fi
+echo "connecting $nodes"
 xpanes -c "kubectl exec -it -n ${pods_patten} {} -- /bin/bash" $nodes
