14299ad53ed3c63d4499f5b80f31b1d67ec30aa6
meteorchen <meteorchen@tencent.com>
2021-08-30 14:51:06 +0800

    fix

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -230,10 +230,12 @@ lvim.builtin.dashboard.active = false
 lvim.builtin.terminal.active = true
 lvim.builtin.which_key.active = true
 
-lvim.builtin.nvimtree.auto_open = 0
-lvim.builtin.nvimtree.side = "left"
-lvim.builtin.nvimtree.disable_window_picker = 1
-lvim.builtin.nvimtree.hide_dotfiles = 0
+if lvim.builtin.nvimtree ~= nil then
+    lvim.builtin.nvimtree.auto_open = 0
+    lvim.builtin.nvimtree.side = "left"
+    lvim.builtin.nvimtree.disable_window_picker = 1
+    lvim.builtin.nvimtree.hide_dotfiles = 0
+end
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = { }
