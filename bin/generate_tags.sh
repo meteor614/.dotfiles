fa44ba0252e8658c19f32d2f9ea4faecf13b9f52
meteor614 <meteor614@gmail.com>
2022-11-21 19:43:10 +0800

    --extra -> --extras

--- bin/generate_tags.sh
+++ bin/generate_tags.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #ctags -R --language-force=c++ --c++-kinds=+plx --fields=+iaS --extra=+q -f ~/cpp_tags /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1
 if type ctags &>/dev/null; then
-    ctags -R --language-force=c++ --c++-kinds=+plx --fields=+iaS --extra=+q -f ~/cpp_tags `echo | g++ -v -x c++ -E - 2>&1 |grep '^ .*/include/c++\(/[^/]*\)\?$'`
+    ctags -R --language-force=c++ --c++-kinds=+plx --fields=+iaS --extras=+q -f ~/cpp_tags `echo | g++ -v -x c++ -E - 2>&1 |grep '^ .*/include/c++\(/[^/]*\)\?$'`
 fi
 #echo | g++ -v -x c++ -E -
