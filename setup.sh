810221e7341ca0b905094fc063790b1d070bf19a
meteor614 <meteor614@gmail.com>
2025-08-30 12:00:31 +0800

    install oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -125,6 +125,15 @@ fi
 if type python3 &>/dev/null; then
     python3 -m pip install python-language-server pynvim &
 fi
+
+# install oh-my-zsh
+if type zsh &>/dev/null; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
 # if type python2 &>/dev/null; then
 #     python2 -m pip install python-language-server pynvim &
 # fi
