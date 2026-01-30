3e23cde9be3fd4c515b0844b541adb9a58e3a62d
meteor614 <meteor614@gmail.com>
2026-01-30 13:05:54 +0800

    add autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,50 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+-- 高亮复制的内容
+autocmd("TextYankPost", {
+  group = augroup("highlight_yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+  end,
+})
+
+-- 保存时删除行尾空格
+autocmd("BufWritePre", {
+  group = augroup("trim_trailing_whitespace", { clear = true }),
+  pattern = "*",
+  callback = function()
+    -- 排除某些文件类型
+    local exclude_filetypes = { "markdown", "diff" }
+    if vim.tbl_contains(exclude_filetypes, vim.bo.filetype) then
+      return
+    end
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
+
+-- 当窗口大小改变时自动调整分割窗口
+autocmd("VimResized", {
+  group = augroup("resize_splits", { clear = true }),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
+
+-- 对于特定文件类型设置更短的更新时间和显示光标行
+autocmd("FileType", {
+  group = augroup("special_filetypes", { clear = true }),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
