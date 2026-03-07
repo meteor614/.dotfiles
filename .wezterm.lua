2844457830ffc1b9c6d58e5ccef81e374f8907d9
meteor614 <meteor614@gmail.com>
2026-03-07 17:03:10 +0800

    key mapping自适应zellij/tmux

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,183 @@
 -- ~/.wezterm.lua
 local wezterm = require 'wezterm'
+local act = wezterm.action
+
+local function basename(path)
+    if not path or path == '' then
+        return nil
+    end
+    local normalized = path:gsub('\\', '/')
+    return normalized:match('([^/]+)$') or normalized
+end
+
+local function current_mux(pane)
+    local process_name = basename(pane:get_foreground_process_name())
+    if not process_name then
+        return nil
+    end
+
+    process_name = process_name:lower()
+    if process_name == 'tmux' or process_name == 'zellij' then
+        return process_name
+    end
+
+    return nil
+end
+
+local function zellij_key_action(key, mods)
+    return act.SendKey {
+        key = key,
+        mods = mods or 'CTRL|ALT',
+    }
+end
+
+local function zellij_tab_number_action(index)
+    local function_keys = { 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9' }
+    return zellij_key_action(function_keys[index])
+end
+
+local function mux_action(actions)
+    return wezterm.action_callback(function(window, pane)
+        local action = actions[current_mux(pane)] or actions.other
+        if action then
+            window:perform_action(action, pane)
+        end
+    end)
+end
+
+local keys = {
+    -- Alt 组合
+    { key = 'f', mods = 'ALT', action = act.SendString '\x1bf' },
+    { key = 'b', mods = 'ALT', action = act.SendString '\x1bb' },
+    { key = 'v', mods = 'ALT', action = act.SendString '\x1bv' },
+    { key = 'i', mods = 'ALT', action = act.SendString '\x1bi' },
+
+    -- Alt-Return 全屏
+    { key = 'Return', mods = 'ALT', action = act.ToggleFullScreen },
+
+    { key = 'n', mods = 'CMD', action = act.SpawnTab 'CurrentPaneDomain' },
+
+    -- Cmd 组合（根据当前前台 multiplexer 自适应）
+    { key = 'w', mods = 'CMD', action = mux_action {
+        tmux = act.SendString '\x02&',
+        zellij = zellij_key_action('w'),
+        other = act.CloseCurrentTab { confirm = true },
+    } },
+    { key = 'd', mods = 'CMD', action = mux_action {
+        tmux = act.SendString '\x02_',
+        zellij = zellij_key_action('d'),
+        other = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    } },
+    { key = 'd', mods = 'CMD|SHIFT', action = mux_action {
+        tmux = act.SendString '\x02-',
+        zellij = zellij_key_action('d', 'CTRL|ALT|SHIFT'),
+        other = act.SplitVertical { domain = 'CurrentPaneDomain' },
+    } },
+    { key = 't', mods = 'CMD', action = mux_action {
+        tmux = act.SendString '\x02c',
+        zellij = zellij_key_action('t'),
+        other = act.SpawnTab 'CurrentPaneDomain',
+    } },
+    { key = ']', mods = 'CMD', action = mux_action {
+        tmux = act.SendString '\x02o',
+        zellij = zellij_key_action('u'),
+    } },
+    { key = '[', mods = 'CMD', action = mux_action {
+        tmux = act.SendString '\x02;',
+        zellij = zellij_key_action('y'),
+    } },
+    { key = ']', mods = 'CMD|SHIFT', action = mux_action {
+        tmux = act.SendString '\x02\x0c',
+        zellij = zellij_key_action('u', 'CTRL|ALT|SHIFT'),
+        other = act.ActivateTabRelative(1),
+    } },
+    { key = '[', mods = 'CMD|SHIFT', action = mux_action {
+        tmux = act.SendString '\x02\x08',
+        zellij = zellij_key_action('y', 'CTRL|ALT|SHIFT'),
+        other = act.ActivateTabRelative(-1),
+    } },
+
+    -- ALT
+    { key = ']', mods = 'ALT', action = act.ActivateTabRelative(1) },
+    { key = '[', mods = 'ALT', action = act.ActivateTabRelative(-1) },
+    { key = '1', mods = 'ALT', action = act.ActivateTab(0) },
+    { key = '2', mods = 'ALT', action = act.ActivateTab(1) },
+    { key = '3', mods = 'ALT', action = act.ActivateTab(2) },
+    { key = '4', mods = 'ALT', action = act.ActivateTab(3) },
+    { key = '5', mods = 'ALT', action = act.ActivateTab(4) },
+    { key = '6', mods = 'ALT', action = act.ActivateTab(5) },
+    { key = '7', mods = 'ALT', action = act.ActivateTab(6) },
+    { key = '8', mods = 'ALT', action = act.ActivateTab(7) },
+    { key = '9', mods = 'ALT', action = act.ActivateTab(8) },
+
+    -- Vi 模式
+    { key = 'c', mods = 'CMD|SHIFT', action = act.ActivateCopyMode },
+    { key = 'c', mods = 'CMD|SHIFT', mode = 'CopyMode', action = act.CopyMode 'Close' },
+
+    -- CMD+Ctrl 组合（Ctrl-b 前缀 + 按键）
+    { key = 't', mods = 'CMD|CTRL', action = act.SendString '\x02\x02c' },
+    { key = 'h', mods = 'CMD|CTRL', action = act.SendString '\x02\x02h' },
+    { key = 'j', mods = 'CMD|CTRL', action = act.SendString '\x02\x02j' },
+    { key = 'k', mods = 'CMD|CTRL', action = act.SendString '\x02\x02k' },
+    { key = 'l', mods = 'CMD|CTRL', action = act.SendString '\x02\x02l' },
+    { key = 'LeftArrow',  mods = 'CMD|CTRL', action = act.SendString '\x02\x02H' },
+    { key = 'DownArrow',  mods = 'CMD|CTRL', action = act.SendString '\x02\x02J' },
+    { key = 'UpArrow',    mods = 'CMD|CTRL', action = act.SendString '\x02\x02K' },
+    { key = 'RightArrow', mods = 'CMD|CTRL', action = act.SendString '\x02\x02L' },
+    { key = ']', mods = 'CMD|CTRL', action = act.SendString '\x02\x02o' },
+    { key = '[', mods = 'CMD|CTRL', action = act.SendString '\x02\x02;' },
+}
+
+for _, mapping in ipairs({
+    { key = 'h', tmux = '\x02h', zellij = 'h', other = 'Left' },
+    { key = 'j', tmux = '\x02j', zellij = 'j', other = 'Down' },
+    { key = 'k', tmux = '\x02k', zellij = 'k', other = 'Up' },
+    { key = 'l', tmux = '\x02l', zellij = 'l', other = 'Right' },
+}) do
+    table.insert(keys, {
+        key = mapping.key,
+        mods = 'CMD',
+        action = mux_action {
+            tmux = act.SendString(mapping.tmux),
+            zellij = zellij_key_action(mapping.zellij),
+            other = act.ActivatePaneDirection(mapping.other),
+        },
+    })
+end
+
+for _, mapping in ipairs({
+    { key = 'LeftArrow', tmux = '\x02H', zellij = 'h', other = { 'Left', 2 } },
+    { key = 'DownArrow', tmux = '\x02J', zellij = 'j', other = { 'Down', 2 } },
+    { key = 'UpArrow', tmux = '\x02K', zellij = 'k', other = { 'Up', 2 } },
+    { key = 'RightArrow', tmux = '\x02L', zellij = 'l', other = { 'Right', 2 } },
+}) do
+    table.insert(keys, {
+        key = mapping.key,
+        mods = 'CMD',
+        action = mux_action {
+            tmux = act.SendString(mapping.tmux),
+            zellij = zellij_key_action(mapping.key, 'CTRL|ALT|SHIFT'),
+            other = act.AdjustPaneSize(mapping.other),
+        },
+    })
+end
+
+for i = 1, 9 do
+    table.insert(keys, {
+        key = tostring(i),
+        mods = 'CMD',
+        action = mux_action {
+            tmux = act.SendString('\x02' .. tostring(i)),
+            zellij = zellij_tab_number_action(i),
+            other = act.ActivateTab(i - 1),
+        },
+    })
+    table.insert(keys, {
+        key = tostring(i),
+        mods = 'CMD|CTRL',
+        action = act.SendString('\x02\x02' .. tostring(i)),
+    })
+end
 
 return {
     -- image_cache_ttl = 3600,
@@ -23,7 +201,7 @@ return {
 
     -- 更快的启动
     skip_close_confirmation_for_processes_named = {
-        'bash', 'sh', 'zsh', 'fish', 'tmux', 'nvim', 'vim'
+        'bash', 'sh', 'zsh', 'fish', 'tmux', 'zellij', 'nvim', 'vim'
     },
 
     automatically_reload_config = true,
@@ -59,83 +237,5 @@ return {
     enable_csi_u_key_encoding = true,
 
     -- 键位映射
-    keys = {
-        -- Alt 组合
-        { key = 'f', mods = 'ALT', action = wezterm.action.SendString '\x1bf' },
-        { key = 'b', mods = 'ALT', action = wezterm.action.SendString '\x1bb' },
-        { key = 'v', mods = 'ALT', action = wezterm.action.SendString '\x1bv' },
-        { key = 'i', mods = 'ALT', action = wezterm.action.SendString '\x1bi' },
-
-        -- Alt-Return 全屏
-        { key = 'Return', mods = 'ALT', action = wezterm.action.ToggleFullScreen },
-
-        { key = 'n', mods = 'CMD', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
-        -- Cmd 组合（tmux 前缀 Ctrl-b + 按键）
-        { key = 'w', mods = 'CMD', action = wezterm.action.SendString '\x02&' },
-        { key = 'd', mods = 'CMD', action = wezterm.action.SendString '\x02_' },
-        { key = 'd', mods = 'CMD|SHIFT', action = wezterm.action.SendString '\x02-' },
-        { key = 't', mods = 'CMD', action = wezterm.action.SendString '\x02c' },
-        { key = 'h', mods = 'CMD', action = wezterm.action.SendString '\x02h' },
-        { key = 'j', mods = 'CMD', action = wezterm.action.SendString '\x02j' },
-        { key = 'k', mods = 'CMD', action = wezterm.action.SendString '\x02k' },
-        { key = 'l', mods = 'CMD', action = wezterm.action.SendString '\x02l' },
-        { key = 'LeftArrow',  mods = 'CMD', action = wezterm.action.SendString '\x02H' },
-        { key = 'DownArrow',  mods = 'CMD', action = wezterm.action.SendString '\x02J' },
-        { key = 'UpArrow',    mods = 'CMD', action = wezterm.action.SendString '\x02K' },
-        { key = 'RightArrow', mods = 'CMD', action = wezterm.action.SendString '\x02L' },
-        { key = ']', mods = 'CMD', action = wezterm.action.SendString '\x02o' },
-        { key = '[', mods = 'CMD', action = wezterm.action.SendString '\x02;' },
-        { key = '1', mods = 'CMD', action = wezterm.action.SendString '\x021' },
-        { key = '2', mods = 'CMD', action = wezterm.action.SendString '\x022' },
-        { key = '3', mods = 'CMD', action = wezterm.action.SendString '\x023' },
-        { key = '4', mods = 'CMD', action = wezterm.action.SendString '\x024' },
-        { key = '5', mods = 'CMD', action = wezterm.action.SendString '\x025' },
-        { key = '6', mods = 'CMD', action = wezterm.action.SendString '\x026' },
-        { key = '7', mods = 'CMD', action = wezterm.action.SendString '\x027' },
-        { key = '8', mods = 'CMD', action = wezterm.action.SendString '\x028' },
-        { key = '9', mods = 'CMD', action = wezterm.action.SendString '\x029' },
-        { key = ']', mods = 'CMD|SHIFT', action = wezterm.action.SendString '\x02\x0c' },
-        { key = '[', mods = 'CMD|SHIFT', action = wezterm.action.SendString '\x02\x08' },
-
-        -- ALT
-        { key = ']', mods = 'ALT', action = wezterm.action.ActivateTabRelative(1) },
-        { key = '[', mods = 'ALT', action = wezterm.action.ActivateTabRelative(-1) },
-        { key = '1', mods = 'ALT', action = wezterm.action.ActivateTab(0) },
-        { key = '2', mods = 'ALT', action = wezterm.action.ActivateTab(1) },
-        { key = '3', mods = 'ALT', action = wezterm.action.ActivateTab(2) },
-        { key = '4', mods = 'ALT', action = wezterm.action.ActivateTab(3) },
-        { key = '5', mods = 'ALT', action = wezterm.action.ActivateTab(4) },
-        { key = '6', mods = 'ALT', action = wezterm.action.ActivateTab(5) },
-        { key = '7', mods = 'ALT', action = wezterm.action.ActivateTab(6) },
-        { key = '8', mods = 'ALT', action = wezterm.action.ActivateTab(7) },
-        { key = '9', mods = 'ALT', action = wezterm.action.ActivateTab(8) },
-
-
-        -- Vi 模式
-        { key = 'c', mods = 'CMD|SHIFT', action = wezterm.action.ActivateCopyMode },
-        { key = 'c', mods = 'CMD|SHIFT', mode = 'CopyMode', action = wezterm.action.CopyMode 'Close' },
-
-        -- CMD+Ctrl 组合（Ctrl-b 前缀 + 按键）
-        { key = 't', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x02c' },
-        { key = 'h', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x02h' },
-        { key = 'j', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x02j' },
-        { key = 'k', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x02k' },
-        { key = 'l', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x02l' },
-        { key = 'LeftArrow',  mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x02H' },
-        { key = 'DownArrow',  mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x02J' },
-        { key = 'UpArrow',    mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x02K' },
-        { key = 'RightArrow', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x02L' },
-        { key = ']', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x02o' },
-        { key = '[', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x02;' },
-        { key = '1', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x021' },
-        { key = '2', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x022' },
-        { key = '3', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x023' },
-        { key = '4', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x024' },
-        { key = '5', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x025' },
-        { key = '6', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x026' },
-        { key = '7', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x027' },
-        { key = '8', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x028' },
-        { key = '9', mods = 'CMD|CTRL', action = wezterm.action.SendString '\x02\x029' },
-    },
+    keys = keys,
 }
-
